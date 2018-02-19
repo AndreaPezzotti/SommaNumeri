4a7fb67f9b38396a83e9036f8afc293702c6230a
Tonsi davide <tonsidavide@gmail.com>
2018-02-19 12:17:42 +0100

    aggiungi somma10

--- src/SommaNumeri.java
+++ src/SommaNumeri.java
@@ -1,4 +1,24 @@
 
-public class SommaNumeri {
-
+public class SommaNumeri 
+{
+	public static int somma4(int x)
+	{
+		for (int i = 0; i < 4; i++) 
+			x++;
+		return x;
+	}
+	
+	public static int somma10(int x)
+	{
+		for (int i = 0; i < 10; i++) 
+			x++;
+		return x;
+	}
+	
+	public static void main(String[] args)
+	{
+		int x=1;
+		System.out.println(somma4(x));
+		System.out.println(somma10(x));
+	}
 }
